149c7d378267a869dbfe7fea0eb184e00446295d
Adam Martinez <amartinez@marketstar.com>
2018-06-26 16:53:43 -0600

    initial comment

---
+++ nightwatch/test_assets/selectors.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+    accountPage: {
+        logo: 'img[class="logoImage"]',
+        signInWithWhichAccountText: 'div[class="groupMargin"]',
+        dasIcon: 'img[alt="DAS Shared Services"]',
+        mksIcon: 'img[alt="MarketStar"]',
+        ccrIcon: 'img[alt="Creative Channel Retail"]',
+        ccsIcon: 'img[alt="Creative Channel Services"]',
+        help: 'a[id="helpDesk"]',
+        dasText: '//*[@id="bySelection"]/div[2]/div/span',
+        mkstxt: '//*[@id="bySelection"]/div[3]/div/span',
+        ccrtxt: '//*[@id="bySelection"]/div[4]/div/span',
+        ccstxt: '//*[@id="bySelection"]/div[5]/div/span'
+    },
+
+    signInPage: {
+        das: {
+            loginMessage: '//*[@id="loginMessage"]',
+            dasUsername: 'input[id="userNameInput"]',
+            dasPassword: 'input[id="passwordInput"]',
+            submitButton: 'span[id="submitButton"]',
+            help: 'a[id="helpDesk"]',
+        }
+    }
+}
---
+++ nightwatch/tests/tests.js
@@ -0,0 +1,48 @@
+const selectors = require('../test_assets/selectors')
+const data = require('../test_assets/data')
+const functions = require('../test_assets/functions')
+
+module.exports = {
+
+    beforeEach: browser => {
+        browser.url('https://prism.dassharedservices.com')
+    },
+
+    after: browser => {
+        browser.end()
+    },
+
+    'Account page UI check': browser => {
+        browser
+            .verify.visible(selectors.accountPage.logo)
+            .verify.containsText(selectors.accountPage.signInWithWhichAccountText, 'Sign in with one of these accounts')
+            .verify.visible(selectors.accountPage.dasIcon)
+            .verify.visible(selectors.accountPage.mksIcon)
+            .verify.visible(selectors.accountPage.ccrIcon)
+            .verify.visible(selectors.accountPage.ccsIcon)
+            .verify.visible(selectors.accountPage.help)
+            .verify.containsText(selectors.accountPage.help, 'Help')
+            .useXpath()
+            .verify.containsText(selectors.accountPage.dasText, 'DAS Shared Services')
+            .verify.containsText(selectors.accountPage.mkstxt, 'MarketStar')
+            .verify.containsText(selectors.accountPage.ccrtxt, 'Creative Channel Retail')
+            .verify.containsText(selectors.accountPage.ccstxt, 'Creative Channel Services')
+            .useCss()
+    },
+
+    'DAS Sign in UI check': browser => {
+        browser
+            .useCss()
+            .verify.visible(selectors.accountPage.logo)
+            .useXpath()
+            // .verify.containsText(selectors.signInPage.das.loginMessage, 'Sign in with your organizational account')
+            .click(selectors.accountPage.dasText)
+            .useCss()
+            .waitForElementVisible(selectors.signInPage.das.dasUsername, 3000)
+            .expect.element(selectors.signInPage.das.dasUsername).to.have.attribute('placeholder').equals('someone@example.com')
+        browser.expect.element(selectors.signInPage.das.dasPassword).to.have.attribute('placeholder').equals('Password')
+        browser
+            .verify.visible(selectors.signInPage.das.submitButton)
+            .verify.containsText(selectors.signInPage.das.submitButton, 'Sign in')
+    }       
+}
